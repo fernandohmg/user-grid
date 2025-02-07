f6c78cefc86a8ce962ac7e83a9f6b825fbf19ed8
Fernando Goncalves <fernando.hmg@gmail.com>
2025-02-07 13:31:44 -0800

    fix: avatar url

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "robohash.org",
       },
+      {
+        protocol: "https",
+        hostname: "dummyjson.com",
+      },
     ],
   },
 };
